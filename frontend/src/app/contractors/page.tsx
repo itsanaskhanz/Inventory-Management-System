1fbdb06038bd5546eec0defae596a9a969c0b037
Anas Khan <anaskhanx101@gmail.com>
2026-08-01 18:07:18 +0500

    feat: add contractors and products pages

---
+++ frontend/src/app/contractors/page.tsx
@@ -0,0 +1,7 @@
+import AppLayout from "@/layouts/AppLayout";
+
+const Page = () => {
+  return <AppLayout>Contractors</AppLayout>;
+};
+
+export default Page;
---
+++ frontend/src/app/products/page.tsx
@@ -0,0 +1,7 @@
+import AppLayout from "@/layouts/AppLayout";
+
+const Page = () => {
+  return <AppLayout>Products</AppLayout>;
+};
+
+export default Page;
